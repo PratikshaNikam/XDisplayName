b31174da3d27fd2ccaf8c376fa891ba063dba015
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-12-04 23:57:05 +0530

    button event added

--- src/DisplayName.jsx
+++ src/DisplayName.jsx
@@ -3,20 +3,33 @@ import React, { useState } from "react";
 function DisplayName() {
   const [name, setName] = useState("");
   const [lastName, setLastName] = useState("");
-  console.log(name);
-  console.log(lastName)
+  const [showName, setShowName] = useState(false);
+  //console.log(name);
+  //console.log(lastName)
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    !name || !lastName ? alert("Please enter your name and last name") : setShowName(true);
+    
+    
+  }
+
+
 return (
   
-    <form style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+    <form style={{ display: "flex", flexDirection: "column", gap: "10px" }} >
     <label>
-      First Name:<input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)} />
+      First Name:<input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
     </label>
 
     <label>
-      Last Name:<input type="text" placeholder="Enter your last name" onChange={(e)=>setLastName(e.target.value)} />
+      Last Name:<input type="text" placeholder="Enter your last name" value={lastName}  onChange={(e)=>setLastName(e.target.value)} />
     </label>
 
-    <button style={{width:"100px"}} >Submit</button>
+    <button type="submit" style={{ width: "100px" }} onClick={handleSubmit}>Submit</button>
+    
+    {showName ? <p>Full Name: {name} {lastName}</p> : null}
   
     </form>
     
