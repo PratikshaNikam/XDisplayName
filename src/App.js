bdcd97bd77d40f179568534b93bcce716b80ace5
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-12-04 17:12:14 +0530

    UI added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DisplayName from './DisplayName';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <DisplayName/>
     </div>
   );
--- src/DisplayName.jsx
+++ src/DisplayName.jsx
@@ -1,9 +1,26 @@
+import React, { useState } from "react";
 
 function DisplayName() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  console.log(name);
+  console.log(lastName)
 return (
-    <div>
-      <h1>Hello, my name is</h1>
-    </div>
+  
+    <form style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+    <label>
+      First Name:<input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)} />
+    </label>
+
+    <label>
+      Last Name:<input type="text" placeholder="Enter your last name" onChange={(e)=>setLastName(e.target.value)} />
+    </label>
+
+    <button style={{width:"100px"}} >Submit</button>
+  
+    </form>
+    
+    
   );
 }
 
